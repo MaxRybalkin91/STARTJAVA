b997dc5a8c5748bc846e51d2d6c5ff4d236c89f9
Maksim Rybalkin <granirock@gmail.com>
2019-02-26 21:47:57 +0300

    Change

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,21 +1,20 @@
 class Variable {
     public static void main(String[] args) {
-    byte cores = 4;
-    short ddr = 16;
-    int ssdGigabytes = 128;
-    long ssdMegabytes = 128000;
-    float frequency = 2.48123f;
-    double diagonal = 15.6;
-    char copyright = 'c';
-    boolean goodLapTop = true;
-    System.out.println(cores);
-    System.out.println(ddr);
-    System.out.println(ssdGigabytes);
-    System.out.println(ssdMegabytes);
-    System.out.println(frequency);
-    System.out.println(diagonal);
-    System.out.println(copyright);
-    System.out.println(goodLapTop);
-
+        byte cores = 4;
+        short ddr = 16;
+        int ssdGigabytes = 128;
+        long ssdMegabytes = 128000;
+        float frequency = 2.48123f;
+        double diagonal = 15.6;
+        char copyright = 'c';
+        boolean goodLapTop = true;
+        System.out.println(cores);
+        System.out.println(ddr);
+        System.out.println(ssdGigabytes);
+        System.out.println(ssdMegabytes);
+        System.out.println(frequency);
+        System.out.println(diagonal);
+        System.out.println(copyright);
+        System.out.println(goodLapTop);
     }
 }
