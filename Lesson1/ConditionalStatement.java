7e35dc7bfaeab4aebeaf541fc6bdea28780e2ef7
MaxRybalkin91 <granirock@gmail.com>
2019-02-26 20:20:22 +0300

    Change

--- Lesson1/ConditionalStatement.java
+++ Lesson1/ConditionalStatement.java
@@ -1,38 +1,38 @@
 class ConditionalStatement {
-  public static void main(String[] args) {
-  int age = 27;
-  boolean male = true;
-  boolean female = false;
-  double height = 1.76;
-  String firstNameLetter = "М";
-  //String firstNameLetter = "И";
+    public static void main(String[] args) {
+    int age = 27;
+    boolean male = true;
+    boolean female = false;
+    double height = 1.76;
+    String firstNameLetter = "М";
+    //String firstNameLetter = "И";
 
-  if(age > 20) {
-    System.out.println("Вам больше 20 лет");
-  }
+    if(age > 20) {
+        System.out.println("Вам больше 20 лет");
+    }
 
-  if(male == true) {
-    System.out.println("Вы - мужчина");
-  }
+    if(male == true) {
+        System.out.println("Вы - мужчина");
+    }
 
-  if(female == false) {
-    System.out.println("Вы не женщина");
-  }
+    if(female == false) {
+        System.out.println("Вы не женщина");
+    }
 
-  if(height < 1.80) {
-    System.out.println("Ваш рост менее 180 сантиметров");
-  } else {
-    System.out.println("Ваш рост более 180 сантиметров");
-  }
+    if(height < 1.80) {
+        System.out.println("Ваш рост менее 180 сантиметров");
+    } else {
+        System.out.println("Ваш рост более 180 сантиметров");
+    }
 
-  if(firstNameLetter == "M") {
-    System.out.println("Ваше имя начинается с буквы \"М\"");
-  }
-  else if(firstNameLetter == "И") {
-    System.out.println("Ваше имя начинается с буквы \"И\"");
-  } else {
-    System.out.println("Ваше имя начинается с неизвестной нам буквы");
-  }
+    if(firstNameLetter == "M") {
+        System.out.println("Ваше имя начинается с буквы \"М\"");
+    }
+    else if(firstNameLetter == "И") {
+        System.out.println("Ваше имя начинается с буквы \"И\"");
+    } else {
+        System.out.println("Ваше имя начинается с неизвестной нам буквы");
+    }
 
-  }
+    }
 }
