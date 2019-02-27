109802f8b7d5d0849109457a9470736aef2d24f8
Maksim Rybalkin <granirock@gmail.com>
2019-02-27 21:13:34 +0300

    Change

--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,29 +1,27 @@
 class Cycle {
     public static void main(String[] args) {
-    for (int x = 0; x <= 20; x++) {
-        System.out.println(x);
-    }
-
-    System.out.println("");
-
-    int y = -6;
-    while (y <= 6) {
-        System.out.println(y);
-        y = y+2;
-    }
-
-    System.out.println("");
-
-    int z = 10;
-    do {
-        if(z%2 == 0) {
-            System.out.print("");
+        for (int i = 0; i <= 20; i++) {
+            System.out.println(i);
         }
-        else if(z%2==1) {
-            System.out.println(z);
+
+        System.out.println("");
+
+        int i = -6;
+        while (i <= 6) {
+            System.out.println(i);
+            i+=2;
         }
-        z++;
-        }
-    while (z<=20);
+
+        System.out.println("");
+
+        i = 10;
+        do {
+            if (i%2 != 0) {
+                System.out.println(i);
+            } else {
+                System.out.print("");
+            }
+            i++;
+        } while (i <= 20);
     }
 }
