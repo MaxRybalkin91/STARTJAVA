760b2b8f901d0232789a0e8d981e808c4ca96ddd
MaxRybalkin91 <47355826+MaxRybalkin91@users.noreply.github.com>
2019-03-02 11:37:01 +0300

    Method "i + "\n"" added

--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,25 +1,23 @@
 class Cycle {
     public static void main(String[] args) {
         for (int i = 0; i <= 20; i++) {
-            System.out.println(i);
+            System.out.print(i + "\n");
         }
 
-        System.out.println("");
+        System.out.print("\n");
 
         int i = -6;
         while (i <= 6) {
-            System.out.println(i);
-            i+=2;
+            System.out.print(i + "\n");
+            i += 2;
         }
 
-        System.out.println("");
+        System.out.print("\n");
 
         i = 10;
         do {
-            if (i%2 != 0) {
-                System.out.println(i);
-            } else {
-                System.out.print("");
+            if (i %2 != 0) {
+                System.out.print(i + "\n");
             }
             i++;
         } while (i <= 20);
