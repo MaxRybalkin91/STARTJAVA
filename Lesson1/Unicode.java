b21be09063695508bfca012f170ca5d8ab955cc7
MaxRybalkin91 <47355826+MaxRybalkin91@users.noreply.github.com>
2019-03-02 12:11:49 +0300

    Add method "unicode++"

--- Lesson1/Unicode.java
+++ Lesson1/Unicode.java
@@ -2,8 +2,7 @@ class Unicode {
     public static void main(String[] args) {
         char unicode = 33;
         while (unicode <= 126) {
-            System.out.println(unicode);
-            unicode++;
+            System.out.print(unicode++ + "\n");
         }
     }
 }
