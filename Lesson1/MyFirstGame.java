0f8fb800a241a3248278e14c475ac8f73a509dfd
Maksim Rybalkin <granirock@gmail.com>
2019-03-02 19:27:16 +0300

    Add changes

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,25 +1,23 @@
 class MyFirstGame {
     public static void main(String[] args) {
         System.out.println("Компьютер загадал число в диапазоне 0-100. Попробуй его отгадать!");
-        int computer = 56;
-        int human;
+        int compNumber = 55; //загаданное компьютером число
+        int playerNumber = 27; //введенное с клавиатуры значение
+        System.out.println("Введите число");
         do {
-            System.out.println("Введите число");
-            human = 23; //здесь должен быть код ввода значения с клавиатуры
-            //при текущем коде программа будет выполняться бесконечно из-за постоянной переменной
-            //при реализации ввода с клавиатуры - каждый раз переменная "human" будет приобретать новое значение
-            if (human < computer) {
+            if (playerNumber < compNumber) {
                 System.out.println("Не угадали!");
                 System.out.println("Введенное вами число меньше того, что загадал компьютер");
-                System.out.println("");
-            } else if (human > computer) {
+                playerNumber = playerNumber + 6; //якобы изменение числа пользователем
+            } else if (playerNumber > compNumber) {
                 System.out.println("Не угадали!");
                 System.out.println("Введенное вами число больше того, что загадал компьютер");
-                System.out.println("");
-            } else {
-                System.out.println("Вы угадали!");
+                playerNumber = playerNumber - 5; //якобы изменение числа пользователем
             }
-        } while (computer != human);
+        } while (compNumber != playerNumber);
+        if (playerNumber == compNumber) {
+            System.out.println("Вы угадали!");
+        }
     }
 }
 
