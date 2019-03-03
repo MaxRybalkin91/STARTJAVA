976d594e0a53cfbba5d21b35e70b3073af80b765
Maksim Rybalkin <granirock@gmail.com>
2019-03-03 13:10:15 +0300

    Change name of "int"

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,51 +1,51 @@
 class Calculator {
     public static void main(String[] args) {
-        int x = 5; //введенное значение
+        int firstInputNumberh = 5; //введенное значение
         char operation = '+';
         //char operation = '-'; вариант введенной операции
         //char operation = '*'; вариант введенной операции
         //char operation = '/'; вариант введенной операции
         //char operation = '^'; вариант введенной операции
         //char operation = '%'; вариант введенной операции
-        int y = 5; //введенное значение
+        int secondInputNumber = 5; //введенное значение
 
         if (operation == '+') {
             System.out.println("Сложение");
-            System.out.println(x + operation + y + "=" + (x + y));
+            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber + secondInputNumber));
         } else if (operation == '-') {
             System.out.println("Вычитание");
-            System.out.println(x + operation + y + "=" + (x - y));
+            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber - secondInputNumber));
         } else if (operation == '*') {
             System.out.println("Умножение");
-            System.out.println(x + operation + y + "=" + (x * y));
+            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber * secondInputNumber));
         } else if (operation == '/') {
-            if (y == 0){
+            if (secondInputNumber == 0){
                 System.out.println("Деление на ноль невозможно!");
             } else {
-                double x1 = x;
-                double y1 = y;
+                double firstInputNumber1 = firstInputNumber;
+                double secondInputNumber1 = secondInputNumber;
                 System.out.println("Деление");
-                System.out.println(x + operation + y + "=" + (x1 / y1));
+                System.out.println(firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber1 / secondInputNumber1));
             }
         } else if (operation == '^') {
             System.out.println("Возведение в степень");
-            if (y >= 0) {
-                int result = x;
-                for (int i = 1; i < y; i++) {
-                    result *= x;
+            if (secondInputNumber >= 0) {
+                int result = firstInputNumber;
+                for (int i = 1; i < secondInputNumber; i++) {
+                    result *= firstInputNumber;
                 }
-            } else if (y < 0) {
-                double x1 = x;
-                double result = x1;
-                for (int i = 0; i >= y; i--) {
-                    result /= x1;
+            } else if (secondInputNumber < 0) {
+                double firstInputNumber1 = firstInputNumber;
+                double result = firstInputNumber1;
+                for (int i = 0; i >= secondInputNumber; i--) {
+                    result /= firstInputNumber1;
                 }
                 System.out.println(result);
             }
-            System.out.println(x + operation + y + "=" + result);
+            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + result);
         } else if (operation == '%') {
             System.out.println("Деление по модулю");
-            System.out.println(x + operation + y + "=" + x % y);
+            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + firstInputNumber % secondInputNumber);
         }
     }
 }
