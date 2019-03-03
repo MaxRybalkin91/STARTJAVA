881425991bc5a183b18ca62f06337194d7713ff6
Maksim Rybalkin <granirock@gmail.com>
2019-03-03 16:47:41 +0300

    Change "String" to "char"

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -1,33 +1,31 @@
 class Calculator {
     public static void main(String[] args) {
         int firstInputNumber = 5;
-        String operation = "+";
-        //String operation = "-"; вариант введенной операции
-        //String operation = "*"; вариант введенной операции
-        //String operation = "/"; вариант введенной операции
-        //String operation = "^"; вариант введенной операции
-        //String operation = "%"; вариант введенной операции
+        char operation = '+';
+        //char operation = '-'; вариант введенной операции
+        //char operation = '*'; вариант введенной операции
+        //char operation = '/'; вариант введенной операции
+        //char operation = '^'; вариант введенной операции
+        //char operation = '%'; вариант введенной операции
         int secondInputNumber = 5;
 
-        if (operation == "+") {
+        if (operation == '+') {
             System.out.println("Сложение");
-            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber + secondInputNumber));
-        } else if (operation == "-") {
+            System.out.println("" + firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber + secondInputNumber));
+        } else if (operation == '-') {
             System.out.println("Вычитание");
-            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber - secondInputNumber));
-        } else if (operation == "*") {
+            System.out.println("" + firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber - secondInputNumber));
+        } else if (operation == '*') {
             System.out.println("Умножение");
-            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber * secondInputNumber));
-        } else if (operation == "/") {
+            System.out.println("" + firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber * secondInputNumber));
+        } else if (operation == '/') {
             if (secondInputNumber == 0){
                 System.out.println("Деление на ноль невозможно!");
             } else {
-                double firstInputNumber1 = firstInputNumber;
-                double secondInputNumber1 = secondInputNumber;
                 System.out.println("Деление");
-                System.out.println(firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber1 / secondInputNumber1));
+                System.out.println("" + firstInputNumber + operation + secondInputNumber + "=" + ((double) firstInputNumber / secondInputNumber1));
             }
-        } else if (operation == "^") {
+        } else if (operation == '^') {
             System.out.println("Возведение в степень");
             if (secondInputNumber >= 0) {
                 int result = firstInputNumber;
@@ -35,16 +33,15 @@ class Calculator {
                     result *= firstInputNumber;
                 }
             } else if (secondInputNumber < 0) {
-                double firstInputNumber1 = firstInputNumber;
-                double result = firstInputNumber1;
+                int result = firstInputNumber;
                 for (int i = 0; i >= secondInputNumber; i--) {
-                    result /= firstInputNumber1;
+                    result /= (double) firstInputNumber;
                 }
-                System.out.println(firstInputNumber + operation + secondInputNumber + "=" + result);
+                System.out.println("" + firstInputNumber + operation + secondInputNumber + "=" + result);
             }
-        } else if (operation == "%") {
+        } else if (operation == '%') {
             System.out.println("Деление по модулю");
-            System.out.println(firstInputNumber + operation + secondInputNumber + "=" + firstInputNumber % secondInputNumber);
+            System.out.println("" + firstInputNumber + operation + secondInputNumber + "=" + (firstInputNumber % secondInputNumber));
         }
     }
 }
