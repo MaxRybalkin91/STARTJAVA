1b41cb2865a8e409108dbf9ac9285e04c92cb7be
Maksim Rybalkin <granirock@gmail.com>
2019-03-03 20:42:52 +0300

    Add exersice from sublevel 2 of lesson 2

---
+++ Lesson2/Person.java
@@ -0,0 +1,23 @@
+class Person {
+    private char gender = 'M';
+    private String name = "Maksim";
+    private short height = 176;
+    private byte weight = 72;
+    private byte age = 27;
+
+    public void go() {
+        System.out.println("Я иду");
+    }
+    public void seat() {
+        System.out.println("Я сижу");
+    }
+    public void run() {
+        System.out.println("Я бегу");
+    }
+    public void speak() {
+        System.out.println("Я разговариваю");
+    }
+    public void study() {
+        System.out.println("Я изучаю Java");
+    }
+}
