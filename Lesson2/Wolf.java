0701944f2b4d352b521ea98b95ff834b925731de
Maksim Rybalkin <granirock@gmail.com>
2019-03-05 00:14:07 +0300

    Add "set" and "get" methods

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -1,9 +1,52 @@
 class Wolf {
-    char gender;
-    String name;
-    byte weight;
-    byte age;
-    String color;
+    private char gender;
+    private String name;
+    private int weight;
+    private int age;
+    private String color;
+
+    public void setGender(char wolfGender) {
+        gender = wolfGender;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setName(String wolfName) {
+        name = wolfName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setWeight(int wolfWeight) {
+        weight = wolfWeight;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setAge(int wolfAge) {
+        age = wolfAge;
+        if (age > 8) {
+            System.out.println("Warning! Incorrect age of wolf!" + "\n");
+        }
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setColor(String wolfColor) {
+        color = wolfColor;
+    }
+
+    public String getColor() {
+        return color;
+    }
 
     void go() {
         System.out.println("Wolf is going");
--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,17 +1,17 @@
 class WolfTest {
     public static void main(String[] args) {
         Wolf wolf = new Wolf();
-        wolf.gender = 'M';
-        wolf.name = "Wolf";
-        wolf.weight = 50;
-        wolf.age = 5;
-        wolf.color = "Dark";
+        wolf.setGender('M');
+        wolf.setName("Wolf");
+        wolf.setWeight(50);
+        wolf.setAge(9);
+        wolf.setColor("Dark");
 
-        System.out.println("Gender of wolf is " + wolf.gender);
-        System.out.println("Name of wolf is " + wolf.name);
-        System.out.println("Weight of wolf is " + wolf.weight + " kg");
-        System.out.println("Age of wolf is " + wolf.age + " years");
-        System.out.println("Color of wolf is " + wolf.color);
+        System.out.println("Gender of wolf is " + wolf.getGender());
+        System.out.println("Name of wolf is " + wolf.getName());
+        System.out.println("Weight of wolf is " + wolf.getWeight() + " kg");
+        System.out.println("Age of wolf is " + wolf.getAge()+ " years");
+        System.out.println("Color of wolf is " + wolf.getColor() + "\n");
 
         wolf.go();
         wolf.seat();
