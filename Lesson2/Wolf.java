9499efe2f4dfda52b38c9a629ba2ed7db630697c
Maksim Rybalkin <granirock@gmail.com>
2019-03-04 19:47:14 +0300

    Add + Change

--- Lesson2/Wolf.java
+++ Lesson2/Wolf.java
@@ -25,26 +25,3 @@ class Wolf {
         System.out.println("Wolf is hunting");
     }
 }
-
-class WolfTest {
-    public static void main(String[] args) {
-        Wolf wolf = new Wolf();
-        wolf.gender = 'M';
-        wolf.name = "Wolf";
-        wolf.weight = 50;
-        wolf.age = 5;
-        wolf.color = "Dark";
-
-        System.out.println("Gender of wolf is " + wolf.gender);
-        System.out.println("Name of wolf is " + wolf.name);
-        System.out.println("Weight of wolf is " + wolf.weight + " kg");
-        System.out.println("Age of wolf is " + wolf.age + " years");
-        System.out.println("Color of wolf is " + wolf.color);
-
-        wolf.go();
-        wolf.seat();
-        wolf.run();
-        wolf.howl();
-        wolf.hunt();
-    }
-}
