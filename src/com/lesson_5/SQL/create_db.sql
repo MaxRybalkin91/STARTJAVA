335f3344b345fb8d270be74d8dfaa64f1f8a3bb5
Maksim Rybalkin <granirock@gmail.com>
2019-04-04 22:47:21 +0300

    Add SQL files

--- src/com/lesson_5/SQL/create_db.sql
+++ src/com/lesson_5/SQL/create_db.sql
@@ -1,12 +1,12 @@
 CREATE TABLE Jaegers (
-    id              int,
-    modelName       text,
-    mark            text,
-    height          int,
-    weight          float,
-    status          text,
-    origin          text,
-    launch          date,
-    kaijuKill       int,
+    id        SERIAL  NOT NULL,
+    modelName TEXT    NOT NULL,
+    mark      TEXT    NOT NULL,
+    height    INTEGER NOT NULL,
+    weight    FLOAT   NOT NULL,
+    status    TEXT    NOT NULL,
+    origin    TEXT    NOT NULL,
+    launch    DATE    NOT NULL,
+    kaijuKill INTEGER NOT NULL,
     PRIMARY KEY (id)
 );
