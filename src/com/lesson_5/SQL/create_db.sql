ab9a02ce52913640a8a695ea233d4a66cb8d9a63
Maksim Rybalkin <granirock@gmail.com>
2019-04-04 19:15:22 +0300

    Add SQL files

---
+++ src/com/lesson_5/SQL/create_db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Jaegers (
+    id              int,
+    modelName       text,
+    mark            text,
+    height          int,
+    weight          float,
+    status          text,
+    origin          text,
+    launch          date,
+    kaijuKill       int,
+    PRIMARY KEY (id)
+);
---
+++ src/com/lesson_5/SQL/init_db.sql
@@ -0,0 +1,11 @@
+INSERT INTO Jaegers (id, modelName , mark, height, weight, status, origin, launch, kaijuKill)
+	VALUES (1, 'Bracer Phoenix', 'Mark-5', 232, 2.128, 'Destroyed', 'China', '01-11-2025', 1),
+           (2, 'Brawler Yukon', 'Mark-1', 245, 2.356, 'Launched', 'USA', '30-01-2015', 1),
+           (3, 'Cherno Alpha', 'Mark-1', 280, 2.412, 'Destroyed', 'Russia', '01-04-2019', 4),
+           (4, 'Coyote Tango', 'Mark-1', 280, 2.312, 'Launched', 'Japan', '30-12-2015', 1),
+           (5, 'November Ajax', 'Mark-6', 259, 2.218, 'Destroyed', 'USA', '01-09-2018', 0),
+           (6, 'Obsidian Fury', 'Mark-4', 280, 3.412, 'Launched', 'China', '25-12-2020', 1),
+           (7, 'Romeo Blue', 'Mark-1', 255, 7.775, 'Destroyed', 'USA', '15-12-2015', 1),
+           (8, 'Gipsy Avenger', 'Mark-6', 268, 2.004, 'Launched', 'USA', '12-01-2034', 1),
+           (9, 'Gipsy Danger', 'Mark-3', 260, 1.980, 'Destroyed', 'USA', '10-07-2017', 9),
+           (10, 'Guardian Bravo', 'Mark-6', 240, 1.975, 'Launched', 'Russia', '15-12-2033', 1);
